1ac43f6e09a3c0ebed4a03dcb0b137ccc9e1dd43
Arthurmp2 <johnson.arthur@gmail.com>
2023-07-21 22:43:06 +0300

    Add files via upload

---
+++ PersonController.groovy
@@ -0,0 +1,104 @@
+package com.artson.eafya
+
+import grails.converters.JSON
+
+class PersonController {
+
+    static allowedMethods = [save: "POST", update: "POST", delete: "POST", list: "GET", show: "GET", update: "PUT"]
+
+    def index = {
+        redirect(action: "list", params: params)
+    }
+
+    def list = {
+        def persons = Person.list()
+        render persons as JSON
+    }
+
+    def create = {
+        def personInstance = new Person()
+        personInstance.properties = params
+        return [personInstance: personInstance]
+    }
+
+    def save = {
+        def personInstance = new Person(params)
+        if (personInstance.save(flush: true)) {
+            flash.message = "${message(code: 'default.created.message', args: [message(code: 'person.label', default: 'Person'), personInstance.id])}"
+            redirect(action: "show", id: personInstance.id)
+        }
+        else {
+            render(view: "create", model: [personInstance: personInstance])
+        }
+        render personInstance as JSON
+    }
+
+    def show = {
+        def personInstance = Person.get(params.id)
+        if (!personInstance) {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'person.label', default: 'Person'), params.id])}"
+            redirect(action: "list")
+        }
+        else {
+            [personInstance: personInstance]
+        }
+        render personInstance as JSON
+    }
+
+    def edit = {
+        def personInstance = Person.get(params.id)
+        if (!personInstance) {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'person.label', default: 'Person'), params.id])}"
+            redirect(action: "list")
+        }
+        else {
+            return [personInstance: personInstance]
+        }
+    }
+
+    def update = {
+        def personInstance = Person.get(params.id)
+        if (personInstance) {
+            if (params.version) {
+                def version = params.version.toLong()
+                if (personInstance.version > version) {
+                    
+                    personInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'person.label', default: 'Person')] as Object[], "Another user has updated this Person while you were editing")
+                    render(view: "edit", model: [personInstance: personInstance])
+                    return
+                }
+            }
+            personInstance.properties = params
+            if (!personInstance.hasErrors() && personInstance.save(flush: true)) {
+                flash.message = "${message(code: 'default.updated.message', args: [message(code: 'person.label', default: 'Person'), personInstance.id])}"
+                redirect(action: "show", id: personInstance.id)
+            }
+            else {
+                render(view: "edit", model: [personInstance: personInstance])
+            }
+        }
+        else {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'person.label', default: 'Person'), params.id])}"
+            redirect(action: "list")
+        }
+    }
+
+    def delete = {
+        def personInstance = Person.get(params.id)
+        if (personInstance) {
+            try {
+                personInstance.delete(flush: true)
+                flash.message = "${message(code: 'default.deleted.message', args: [message(code: 'person.label', default: 'Person'), params.id])}"
+                redirect(action: "list")
+            }
+            catch (org.springframework.dao.DataIntegrityViolationException e) {
+                flash.message = "${message(code: 'default.not.deleted.message', args: [message(code: 'person.label', default: 'Person'), params.id])}"
+                redirect(action: "show", id: params.id)
+            }
+        }
+        else {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'person.label', default: 'Person'), params.id])}"
+            redirect(action: "list")
+        }
+    }
+}
